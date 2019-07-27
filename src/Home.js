b5d90e05783c74f9580022f0edfc456efdeaf48a
danyentezari <dany.entezari@gmail.com>
2019-07-27 11:48:35 +0400

    Introduced context.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import Jumbotron from './Jumbotron';
 import NavBar from './NavBar';
 import JigSaw from './JigSaw';
@@ -6,73 +6,77 @@ import SignUpWindow from './SignUpWindow'
 import { InfoGroup, Info } from './InfoGroup'
 import { Avatar } from './AvatarGroup'  
 
+const AppContext = createContext();
+
 const Home = () => {
 
   const [state, setState] = useState({ signUpForm: false });
 
   return (
-    <div className="App">
 
-      <NavBar />
-      <Jumbotron img="img/bg-masthead.jpg"/>
-      <InfoGroup>
-        <Info 
-          iconClass="icon icon-screen-desktop" 
-          title="Fully" 
-          caption="This theme will look great on any device, no matter the size!"/>
-        <Info 
-          iconClass="icon icon-layers" 
-          title="Bootstrap 4 Ready" 
-          caption="Featuring the latest build of the new Bootstrap 4 framework!"/>
-        <Info 
-          iconClass="icon icon-check" 
-          title="Easy to Use" 
-          caption="Easy to Use"/>
-      </InfoGroup>
+    <AppContext.Provider>
+      <div className="Home">
+        <NavBar />
+        <Jumbotron img="img/bg-masthead.jpg"/>
+        <InfoGroup>
+          <Info 
+            iconClass="icon icon-screen-desktop" 
+            title="Fully" 
+            caption="This theme will look great on any device, no matter the size!"/>
+          <Info 
+            iconClass="icon icon-layers" 
+            title="Bootstrap 4 Ready" 
+            caption="Featuring the latest build of the new Bootstrap 4 framework!"/>
+          <Info 
+            iconClass="icon icon-check" 
+            title="Easy to Use" 
+            caption="Easy to Use"/>
+        </InfoGroup>
 
-      <JigSaw 
-        order={2}
-        img="img/bg-showcase-1.jpg"
-        title="Fully Responsive Design"
-        caption="When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!"
-      />
-      
-      <JigSaw 
-        order={1}
-        img="img/bg-showcase-2.jpg"
-        title="Fully Responsive Design"
-        caption="When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!"
-      />
-      
-      <JigSaw 
-        order={2}
-        img="img/bg-showcase-3.jpg"
-        title="Easy to Use & Customize"
-        caption="Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!"
-      />
-      
-      <InfoGroup className="testimonials" title="What people are saying...">
-        <Avatar 
-          img="img/testimonials-1.jpg"
-          title="Margaret E."
-          caption={"This is fantastic! Thanks so much guys!"}
+        <JigSaw 
+          order={2}
+          img="img/bg-showcase-1.jpg"
+          title="Fully Responsive Design"
+          caption="When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!"
         />
-        <Avatar 
-          img="img/testimonials-2.jpg"
-          title="Fred S."
-          caption={"Bootstrap is amazing. I\'ve been using it to create lots of super nice landing pages."}
+        
+        <JigSaw 
+          order={1}
+          img="img/bg-showcase-2.jpg"
+          title="Fully Responsive Design"
+          caption="When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!"
         />
-        <Avatar 
-          img="img/testimonials-3.jpg"
-          title="Sarah W."
-          caption={"Thanks so much for making these free resources available to us!"}
+        
+        <JigSaw 
+          order={2}
+          img="img/bg-showcase-3.jpg"
+          title="Easy to Use & Customize"
+          caption="Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!"
         />
-      </InfoGroup>
+        
+        <InfoGroup className="testimonials" title="What people are saying...">
+          <Avatar 
+            img="img/testimonials-1.jpg"
+            title="Margaret E."
+            caption={"This is fantastic! Thanks so much guys!"}
+          />
+          <Avatar 
+            img="img/testimonials-2.jpg"
+            title="Fred S."
+            caption={"Bootstrap is amazing. I\'ve been using it to create lots of super nice landing pages."}
+          />
+          <Avatar 
+            img="img/testimonials-3.jpg"
+            title="Sarah W."
+            caption={"Thanks so much for making these free resources available to us!"}
+          />
+        </InfoGroup>
 
-      <Jumbotron img="img/bg-masthead.jpg"/>
+        <Jumbotron img="img/bg-masthead.jpg"/>
 
-      <SignUpWindow />
-    </div>
+        {state.signUpForm && <SignUpWindow />}
+      </div>
+    </ AppContext.Provider>
   );
 }
 
